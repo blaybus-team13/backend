88e2db23f2e71874ca5981202a994dc31698bdeb
yoonhwan.oh <oyhoyhk@naver.com>
2025-02-16 09:53:42 +0000

    feature : 센터 관리자, 어르신 스키마 수정

--- models/CenterAdmin.js
+++ models/CenterAdmin.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const centerAdminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   userid: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   tel: { type: String, required: true, unique: true },
   position: { type: String, required: true },
   address: { type: String, required: true },
-  profileImage: { type: String, required: false },
   center: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Center",
--- models/Senior.js
+++ models/Senior.js
@@ -1,21 +1,129 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const seniorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  tel: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
-  profileImage: { type: String, required: false },
-  services: { type: [String], required: true },
-  workingDays: { type: [String], required: true },
-  workingHours: { type: [Number], required: true },
-  minSalary: { type: Number, required: true },
-  info: {
-    gender: { type: String, required: true },
-    weight: { type: Number, required: true },
-    hasCohabitant: { type: Boolean, required: true },
+const seniorSchema = new Schema(
+  {
+    seniorId: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["남성", "여성성"],
+      required: true,
+    },
+    careGrade: {
+      type: String,
+      enum: [
+        "등급없음",
+        "1등급",
+        "2등급",
+        "3등급",
+        "4등급",
+        "5등급",
+        "인적지원 등급",
+      ],
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    diseaseInfo: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    careDays: {
+      type: [
+        {
+          type: String,
+          enum: ["월", "화", "수", "목", "금", "토", "일"],
+        },
+      ],
+      required: true,
+    },
+    careStartTime: {
+      type: Date,
+      required: true,
+    },
+    careEndTime: {
+      type: Date,
+      required: true,
+    },
+    additionalInfo: {
+      type: [
+        {
+          type: String,
+          enum: [
+            "가사도우미 있음",
+            "주차가능",
+            "반려동물 있음",
+            "집 평수 30평 이상",
+          ],
+        },
+      ],
+      required: false,
+    },
+    mealAssistance: {
+      type: [{
+          type: String,
+          enum: ["스스로 식사 가능", "식사 차려드리기" ,"죽, 반찬 등 조리 필요", "경관식 보조"]
+        }],
+      required: true,
+    },
+    toiletAssistance: {
+      type: [{
+          type: String,
+          enum: ["스스로 배변 가능", "가끔 대소변 실수 시 도움", "기저귀 케어 필요", "유치도뇨/방광루/장루 관리"]
+        }],
+      required: true,
+    },
+    mobilityAssistance: {
+      type: [{
+          type: String,
+          enum: ["스스로 거동 가능", "이동시 부축 도움", "휠체어 이동 보조", "거동 불가"]
+        }],
+      required: true,
+    },
+    dailyLifeService: {
+      type: [{
+          type: String,
+          enum: ["청소, 빨래 보조", "목욕 보조", "병원 동행행", "산책, 간단한 운동", "말벗 등 정서지원", "인지자극 활동"]
+        }],
+      required: true,
+    },
+    additionalService: {
+      type: String,
+      required: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  {
+    timestamps: true,
+    collection: "seniors",
+  }
+);
 
-module.exports = mongoose.model("Senior", seniorSchema);
+const Senior = mongoose.model("Senior", seniorSchema);
 
-// 나이, 등급, 등급 정보, 근무지, 담당자 연락처(tel은 어르신 연락처)
+module.exports = Senior;
